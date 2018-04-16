9207d8c345a03672c03637bd5cffdf767a3406b9
Marco Rambaldi <marco.rambaldi@student.unife.it>
2018-04-16 17:33:18 +0200

    implementato il server networkhandler del peer

--- peer/LocalData.py
+++ peer/LocalData.py
@@ -12,6 +12,9 @@ class LocalData:
 	# 'pktid'
 	sent_packet = str()
 
+	# {'pktid' : (ip, port)}
+	received_packets = dict()
+
 	# ('ipv4', 'ipv6', 'port')
 	superpeer_candidates = list()
 
@@ -59,4 +62,18 @@ class LocalData:
 	@classmethod
 	def get_sent_packet(cls) -> str:
 		return cls.sent_packet
+	# -----------------------------------------------------------------------------
+
+	# received packets management --------------------------------------------------
+	@classmethod
+	def add_received_packet(cls, pktid: str, ip_peer: str, port_peer: int) -> None:
+		cls.received_packets[pktid] = (ip_peer, port_peer)
+
+	@classmethod
+	def delete_received_packet(cls, pktid: str) -> None:
+		del cls.received_packets[pktid]
+
+	@classmethod
+	def exist_in_received_packets(cls, pktid: str) -> bool:
+		return pktid in cls.received_packets
 	# -----------------------------------------------------------------------------
--- peer/handler/NetworkHandler.py
+++ peer/handler/NetworkHandler.py
@@ -2,13 +2,42 @@
 
 import socket
 from common.HandlerInterface import HandlerInterface
-from utils import Logger
+from utils import Logger, net_utils
+from peer.LocalData import LocalData
+from threading import Timer
 
 
 class NetworkHandler(HandlerInterface):
 
 	def __init__(self, log: Logger.Logger):
 		self.log = log
+
+	def __delete_packet(self, pktid: str) -> None:
+		""" Delete a packet from the net
+
+		:param pktid: id of the packet
+		:return: None
+		"""
+		if LocalData.exist_in_received_packets(pktid):
+			LocalData.delete_received_packet(pktid)
+
+	def __forward_packet(self, ttl: str, packet: str) -> None:
+		""" Forward a supe packet in the net to superpeer
+
+		:param ip_sender: ip address of sender host
+		:param ttl: packet time to live
+		:param packet: string representing the packet
+		:return: None
+		"""
+		new_ttl = int(ttl) - 1
+
+		if new_ttl > 0:
+			packet = packet[:80] + str(new_ttl).zfill(2) + packet[82:]
+			superpeer_ip4 = LocalData.get_superpeer_ip4()
+			superpeer_ip6 = LocalData.get_superpeer_ip6()
+			superpeer_port = LocalData.get_superpeer_port()
+
+			net_utils.send_packet_and_close(superpeer_ip4, superpeer_ip6, superpeer_port, packet)
 
 	def serve(self, sd: socket.socket) -> None:
 		""" Handle the neighbours packet
@@ -33,7 +62,30 @@ class NetworkHandler(HandlerInterface):
 		command = packet[:4]
 
 		if command == "SUPE":
-			pass
+			if len(packet) != 82:
+				self.log.write_red('Invalid packet. Unable to reply.')
+				sd.close()
+				return
+
+			pktid = packet[4:20]
+			ip_peer = packet[20:75]
+			port_peer = int(packet[75:80])
+			ttl = packet[80:82]
+			sd.close()
+
+			# packet management
+			if pktid == LocalData.get_sent_packet():
+				return
+
+			if not LocalData.exist_in_received_packets(pktid):
+				LocalData.add_received_packet(pktid, ip_peer, port_peer)
+				t = Timer(20, function=self.__delete_packet, args=(pktid,))
+				t.start()
+			else:
+				return
+
+			# forwarding the supe packet to superpeer
+			self.__forward_packet(ttl, packet)
 
 		else:
 			self.log.write_red('Invalid packet. Unable to reply.')
