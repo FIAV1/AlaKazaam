d859c837549a528ae0fc9529bb6f2e6e2444267b
Marco Rambaldi <marco.rambaldi@student.unife.it>
2018-04-18 17:28:38 +0200

    minor fix: possibilità di scaricare nel flusso LISTFILES

--- superpeer/handler/MenuHandler.py
+++ superpeer/handler/MenuHandler.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 from utils import net_utils, Logger, shell_colors as shell
+from utils.Downloader import Downloader
 from superpeer.LocalData import LocalData
 from superpeer.database import database
 from superpeer.model import peer_repository, file_repository
@@ -107,6 +108,48 @@ class MenuHandler:
 				print(f'Error: {e}')
 				print('The server has encountered an error while trying to serve the request.')
 				return
+
+			while True:
+					index = input('\nPlease select a file to download: ')
+
+					if index == 'q':
+						print('\n')
+						LocalData.clear_peer_files()
+						return
+
+					try:
+						index = int(index) - 1
+
+					except ValueError:
+						shell.print_red(f'\nWrong index: number in range 1 - {len(files)} expected.')
+						continue
+
+					if 0 <= index <= len(files):
+
+						chosen_peer_file = LocalData.get_peer_file_by_index(index)
+						host_ip4 = LocalData.get_file_owner_ip4(chosen_peer_file)
+						host_ip6 = LocalData.get_file_owner_ip6(chosen_peer_file)
+						host_port = LocalData.get_file_owner_port(chosen_peer_file)
+						file_md5 = LocalData.get_file_md5(chosen_peer_file)
+						file_name = LocalData.get_file_name(chosen_peer_file)
+
+						# preparo packet per retr, faccio partire server in attesa download, invio packet e attendo
+						packet = 'RETR' + file_md5
+
+					try:
+						Downloader(host_ip4, host_ip6, host_port, packet, file_name).start()
+
+						shell.print_green(f'\nDownload of {file_name} completed.\n')
+						LocalData.clear_peer_files()
+						# LocalData.add_shared_file(file_name, file_md5, os.stat('shared/' + file_name).st_size)
+
+					except OSError:
+						shell.print_red(f'\nError while downloading {file_name}\n')
+						break
+
+					else:
+						shell.print_red(f'\nWrong index: number in range 1 - {len(files)} expected.')
+						continue
 
 		else:
 			pass
