1c124cb30afe4df73a5d978786eefacacc37bab7
Giovanni Fiorini <giovanni.fiorini@student.unife.it>
2018-04-19 17:07:58 +0200

    Fix alle stampe con **, end=''**

--- superpeer/handler/MenuHandler.py
+++ superpeer/handler/MenuHandler.py
@@ -92,15 +92,15 @@ class MenuHandler:
 
 			dir_file = list()
 
-			for count, dir_entry in enumerate(os.scandir('shared'), 1):
-				dir_file.append((dir_entry.name, hasher.get_md5(dir_entry.path), dir_entry.stat().st_size))
-				print(f'\n{count}) {dir_entry.name}')
-				shell.print_yellow(f' {hasher.get_md5(dir_entry.path)}')
-				print(f' size: {dir_entry.stat().st_size}')
-
-			if count == 0:
+			if not os.scandir('shared'):
 				shell.print_yellow('\nNo file in the shared folder.')
 				return
+
+			for count, dir_entry in enumerate(os.scandir('shared'), 1):
+				dir_file.append((dir_entry.name, hasher.get_md5(dir_entry.path), dir_entry.stat().st_size))
+				print(f'\n{count}) {dir_entry.name}', end='')
+				shell.print_yellow(f' {hasher.get_md5(dir_entry.path)}', end='')
+				print(f' size: {dir_entry.stat().st_size}')
 
 			while True:
 
@@ -108,7 +108,6 @@ class MenuHandler:
 
 				if index == 'q':
 					print('\n')
-					dir_file.clear()
 					return
 
 				try:
@@ -130,8 +129,6 @@ class MenuHandler:
 					else:
 						shell.print_yellow('\nThe file is already shared.')
 
-					dir_file.clear()
-
 					break
 				else:
 					shell.print_red(f'\nWrong index: number in range 1 - {count} expected.')
@@ -139,14 +136,14 @@ class MenuHandler:
 
 		elif choice == "DEFF":
 
-			for count, file in enumerate(LocalData.get_shared_files()):
-				print(f'\n{count}) {LocalData.get_shared_filename(file)}')
-				shell.print_yellow(f' {LocalData.get_shared_filemd5(file)}')
-				print(f' size: {LocalData.get_shared_dim(file)}')
-
-			if count == 0:
+			if not LocalData.get_shared_files():
 				shell.print_yellow('\nNo file in sharing.')
 				return
+
+			for count, file in enumerate(LocalData.get_shared_files(), 1):
+				print(f'\n{count}) {LocalData.get_shared_filename(file)}', end='')
+				shell.print_yellow(f' {LocalData.get_shared_filemd5(file)}', end='')
+				print(f' size: {LocalData.get_shared_dim(file)}')
 
 			while True:
 
@@ -166,8 +163,8 @@ class MenuHandler:
 
 					deleted_file = LocalData.get_shared_file_by_index(index)
 
-					shell.print_blue(f'\n{LocalData.get_shared_filename(deleted_file)}')
-					shell.print_yellow(f' {LocalData.get_shared_filemd5(deleted_file)}')
+					shell.print_blue(f'\n{LocalData.get_shared_filename(deleted_file)}', end='')
+					shell.print_yellow(f' {LocalData.get_shared_filemd5(deleted_file)}', end='')
 					shell.print_blue(' removed from sharing.')
 
 					break
@@ -180,9 +177,10 @@ class MenuHandler:
 			while True:
 				search = input('\nEnter the file name: ')
 
-				if not 0 <= len(search) <= 20:
+				if not 0 < len(search) <= 20:
 					shell.print_red('\nFile name must be between 1 and 20 chars long.\n')
 					continue
+
 				break
 
 			# Read matching files from DB
@@ -213,8 +211,8 @@ class MenuHandler:
 
 							ip4_peer, ip6_peer = net_utils.get_ip_pair(peer.ip)
 							# stampa di debug
-							# print(f'\n{LocalData.peer_file_index(ip4_peer, ip6_peer, peer.port, file.file_md5, file.file_name)}')
-							# shell.print_yellow(f' {file.file_md5}')
+							# print(f'\n{LocalData.peer_file_index(ip4_peer, ip6_peer, peer.port, file.file_md5, file.file_name)}', end='')
+							# shell.print_yellow(f' {file.file_md5}', end='')
 							# print(f' {file.file_name} from {ip4_peer|ip6_peer} on port {peer.port}')
 							LocalData.add_peer_files(ip4_peer, ip6_peer, peer.port, file.file_md5, file.file_name)
 
@@ -271,7 +269,7 @@ class MenuHandler:
 				shell.print_yellow('\nNo matching results from the superpeer network.\n')
 
 			if len(peer_files) < 1:
-				shell.print_red('\nNo file matching the keyword.\n')
+				shell.print_yellow('\nNo file matching the keyword.\n')
 				return
 
 			for peer_file in peer_files:
@@ -282,9 +280,9 @@ class MenuHandler:
 				file_md5 = LocalData.get_file_md5(peer_file)
 				file_name = LocalData.get_file_name(peer_file)
 
-				print(f'\n{LocalData.peer_file_index(peer_ip4, peer_ip6, peer_port, file_md5, file_name) + 1}')
-				shell.print_yellow(f' {file_md5}')
-				shell.print_blue(f' {file_name}')
+				print(f'\n{LocalData.peer_file_index(peer_ip4, peer_ip6, peer_port, file_md5, file_name) + 1}', end='')
+				shell.print_yellow(f' {file_md5}', end='')
+				shell.print_blue(f' {file_name}', end='')
 				print(f' from {peer_ip4}|{peer_ip6} on port {peer_port}')
 
 			while True:
@@ -319,7 +317,6 @@ class MenuHandler:
 
 						shell.print_green(f'\nDownload of {file_name} completed.\n')
 						LocalData.clear_peer_files()
-						# LocalData.add_shared_file(file_name, file_md5, os.stat('shared/' + file_name).st_size)
 					except OSError:
 						shell.print_red(f'\nError while downloading {file_name}\n')
 
