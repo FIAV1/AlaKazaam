9023a3c765fd556282d46e1eb1c4c97efc81a55b
federico <ffrigo8@gmail.com>
2018-04-19 18:42:08 +0200

    File download enhancements

--- peer/handler/MenuHandler.py
+++ peer/handler/MenuHandler.py
@@ -21,14 +21,19 @@ class MenuHandler:
 		p_ssid = LocalData.session_id
 
 		if choice == "ADFF":
+			# check se shared dir esiste
+			if not os.path.exists('shared'):
+				shell.print_red('\nCannot find the shared folder.')
+				return
+
+			# se non ci sono file nella shared dir avvisa ed esce
+			if not os.scandir('shared'):
+				shell.print_yellow('No file available for sharing. Add files to shared dir to get started.\n')
+				return
+
 			temp_files = []
 			shell.print_blue('\nFiles available for sharing:')
-			for count, file in enumerate(os.scandir('shared')):
-				# se non ci sono file nella shared dir avvisa ed esce
-				if len(os.scandir('shared')):
-					shell.print_yellow('No file available for sharing. Add files to shared dir to get started.\n')
-					return
-
+			for count, file in enumerate(os.scandir('shared'), 1):
 				# stampa i risultati della scandir
 				file_md5 = hasher.get_md5(f'shared/{file.name}')
 				print(f'{count}] {file.name} | {file_md5}')
@@ -78,14 +83,14 @@ class MenuHandler:
 			# recupera i files in sharing
 			files = LocalData.get_shared_files()
 
+			# check se ci sono file in sharing
+			if not files:
+				shell.print_yellow('No files currently in sharing. Add files choosing the command from the menu.\n')
+				return
+
 			# scelta del file da rimuovere
 			shell.print_blue('\nFile currently in sharing:')
-			for count, file in enumerate(files, start=1):
-				# check se ci sono file in sharing
-				if not files:
-					shell.print_yellow('No files currently in sharing. Add files choosing the command from the menu.\n')
-					return
-
+			for count, file in enumerate(files, 1):
 				print(f'{count}] {LocalData.get_shared_file_name(file)} | {LocalData.get_shared_file_md5(file)}')
 
 			while True:
@@ -160,6 +165,7 @@ class MenuHandler:
 				return
 
 			downloadables = []
+			sources = []
 			try:
 				num_downloadables = int(socket.recv(3))
 
@@ -189,6 +195,7 @@ class MenuHandler:
 					try:
 						(source_ip4, source_ip6) = net_utils.get_ip_pair(socket.recv(55).decode())
 						source_port = int(socket.recv(5))
+						sources.append((source_ip4, source_ip6, source_port))
 					# TODO: da rimuovere dopo i test (?)
 					except net_utils.socket.error:
 						shell.print_red(f'\nError while receiving the response from the superpeer: {sp_ip4}|{sp_ip6} [{sp_port}].\n')
@@ -196,16 +203,17 @@ class MenuHandler:
 					except ValueError:
 						shell.print_red(f'\nInvalid packet from superpeer: {sp_ip4}|{sp_ip6} [{sp_port}].\n')
 						continue
-
+				if sources:
 					# prepara una lista con tutti i file che possono essere scaricati
-					downloadables.append((file_name, file_md5, source_ip4, source_ip6, source_port))
+					downloadables.append((file_name, file_md5, sources))
 
-			shell.print_blue(f'\nFiles found:')
-			for count, downloadable in enumerate(downloadables, start=1):
-				print(f'{count}]', end='')
-				shell.print_blue(f'{downloadable[0]}|{downloadable[1]}', end='')
-				print(' <=> ', end='')
-				shell.print_yellow(f'{sp_ip4}|{sp_ip6} [{sp_port}]')
+			if not downloadables:
+				shell.print_red(f'\nSomething went wrong while retrieving {search}\n')
+				return
+
+			shell.print_green(f'\nFiles found:')
+			for count, downloadable in enumerate(downloadables, 1):
+				print(f'{count}] {downloadable[0]} | {downloadable[1]}')
 
 			while True:
 				index_d = input('Choose a file to download (q to cancel): ')
@@ -220,17 +228,43 @@ class MenuHandler:
 					shell.print_red(f'\nWrong index: number in range 1 - {len(downloadables)} expected\n')
 					continue
 
-				if 0 <= index_d <= len(downloadables) - 1:
-					packet = "RETR" + downloadables[index_d][1]
-					try:
-						Downloader(downloadables[index_d][2], downloadables[index_d][3], downloadables[index_d][4], packet, downloadables[index_d][0])
-						shell.print_green(f'\nDownload di {downloadables[index_d][0]} completato con successo.\n')
-					except OSError:
-						shell.print_red(f'\nError while downloading {downloadables[index_d][0]}.\n')
-					break
-				else:
+				if not 0 <= index_d <= len(downloadables) - 1:
 					shell.print_red(f'\nWrong index: number in range 1 - {len(downloadables)} expected\n')
 					continue
+				else:
+					available_file_name = downloadables[index_d][0]
+					available_file_md5 = downloadables[index_d][1]
+					availables = downloadables[index_d][2]
+
+					shell.print_green(f'\nAvailable sources for {available_file_name}:')
+					for count, available in enumerate(availables, 1):
+						print(f'{count}] {available[0]}|{available[1]} [{available[2]}]')
+
+					while True:
+						index_a = input(f'Choose a source for {available_file_name} (q to cancel): ')
+
+						if index_a == "q":
+							print('\n')
+							return
+
+						try:
+							index_a = int(index_a) - 1
+						except ValueError:
+							shell.print_red(f'\nWrong index: number in range 1 - {len(availables)} expected\n')
+							continue
+
+						if 0 <= index_a <= len(availables) - 1:
+							packet = "RETR" + available_file_md5
+							try:
+								Downloader(availables[index_a][0], availables[index_a][1], availables[index_a][2], packet, available_file_name)
+								shell.print_green(f'\n{available_file_name} downloaded successfully.\n')
+							except OSError:
+								shell.print_red(f'\nError while downloading {available_file_name}.\n')
+							break
+						else:
+							shell.print_red(f'\nWrong index: number in range 1 - {len(availables)} expected\n')
+							continue
+					break
 
 		elif choice == "SHOWSUPER":
 			print(f'\nYour current superpeer is: {sp_ip4}|{sp_ip6} [{sp_port}]\n')
